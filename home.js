fa79bf62f40794aecf20926cb5cea657929025e8
vaibhav-372 <133035287+vaibhav-372@users.noreply.github.com>
2024-07-18 00:33:30 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
+      <h1 className="text-5xl font-medium">Home</h1>
+      <p className="text-blue-700 font-medium text-3xl pt-7">Welcome to the chat app!</p>
+    </div>
+  );
+}
+
+
+export default Home;
